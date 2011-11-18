3259ecd83f8282842e39496fae83604be31757a8
Darren K Maczka <dmaczka@vt.edu>
2011-11-17 21:28:19 -0500

    missing 'request' argument to origins view

--- SnB/coffee/views.py
+++ SnB/coffee/views.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta
 from SnB.coffee.models import Origin
 # Create your views here.
 
-def origins():
+def origins(request):
     origin_list = Origin.objects.all()
     c = {'origin_list': origin_list}
     return render_to_response('orgins.html', c)
