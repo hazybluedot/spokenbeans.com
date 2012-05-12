17d489e843a1887bcf1164f63d929cc856b1ceed
Darren Maczka <dmaczka@vt.edu>
2012-05-12 14:10:13 -0400

    Added a getCyclistPrice function
    to calcualte cyclist pick wholesale price

--- SnB/coffee/prices.py
+++ SnB/coffee/prices.py
@@ -1,5 +1,15 @@
+from SnB.coffee.models import Origin
+import numpy as np
+
 def getCyclistPrice():
-    return (0,0)
+    origin_list = Origin.objects.filter(blend=False, available=True, decaf=False)
+    prices = []
+    for origin in origin_list:
+        prices.append(origin.wholesaleprice)
+    avg = np.average(prices)
+    std = np.std(prices)
+    cyclist_wholesale = np.min(np.max(prices),avg+0.5*std)
+    return cyclist_wholesale
 
 def getRetailPrice(wholesale, weight, markup):
     "Given wholesale price, weight (float),  markup percent, calculate retail price"
@@ -7,6 +17,12 @@ def getRetailPrice(wholesale, weight, markup):
     weight = float(weight)
     markup = float(markup)
     return round(weight*wholesale*(markup+1),2)
+
+def getBothPrices(wholesale):
+    price_list = ()
+    price_list += ( addTax(getRetailPrice(float(wholesale), 0.77, 0.75), 0.025), )
+    price_list += ( addTax(getRetailPrice(float(wholesale), 0.38, 0.85), 0.025), )
+    return price_list
 
 def addTax(price, tax):
     price = float(price)
